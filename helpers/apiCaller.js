de3b3b739c9d96934b0324499354a02134ce49ac
Gabriel Paredes <gparedes13092000@gmail.com>
2023-09-29 20:11:08 -0600

    CAMBIOS

--- helpers/apiCaller.js
+++ helpers/apiCaller.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 
-const url = "http://127.0.0.1:5000/api"
+// const url = "http://127.0.0.1:5000/api"
+const url = "http://gabrielparedes2000.pythonanywhere.com//api"
 
 const getDataById = async (endpoint, id) => {
   let response
